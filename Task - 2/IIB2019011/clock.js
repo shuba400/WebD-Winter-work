1795f7eadba3c932e982b30be64979507da7b4f3
shuba400 <51315650+shuba400@users.noreply.github.com>
2020-01-01 13:33:14 +0530

    IIB2019011

---
+++ Task - 2/IIB2019011/clock.js
@@ -0,0 +1,27 @@
+@import url('https://fonts.googleapis.com/css?family=Anton|Bangers&display=swap');
+
+body {
+  background-image: url("background.jpg");
+}
+
+.digital_clock{
+  color: #66ff99;
+  font-family: 'Anton', sans-serif;
+  font-size: 56px;
+  text-align: center;
+  padding-top: 40px;
+}
+
+.date{
+	color: #66ff99;
+  	font-size: 20px;
+  	font-family: 'Bangers', cursive;
+  	font-weight: 100;
+  	text-align: center;
+  	padding-bottom: 40px;
+}
+
+.container{
+	margin-top: 15%;
+}
+
