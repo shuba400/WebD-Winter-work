662d3ef43be9b746db6c2a332a10f76d53d9cb2f
shuba400 <51315650+shuba400@users.noreply.github.com>
2020-01-01 13:35:09 +0530

    Add files via upload

--- Task - 2/IIB2019011/clock.js
+++ Task - 2/IIB2019011/clock.js
@@ -1,27 +1,45 @@
-@import url('https://fonts.googleapis.com/css?family=Anton|Bangers&display=swap');
+function current_time(){
+	var date = new Date();
+	var hour = date.getHours();
+  	var min = date.getMinutes();
+    var sec = date.getSeconds();
+    var sess = "AM";
+    var day = date.getDay(); //(0-6)
+    var dated = date.getDate(); //(1-31)
+    var year = date.getFullYear(); //YYYY
+    var month = date.getMonth() + 1; //(0-11)
 
-body {
-  background-image: url("background.jpg");
+    day = correct_day(day);
+
+    if(hour >= 12) sess = "PM";
+    else sess = "AM";
+
+    hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12): hour);
+
+    hour = formatted_time(hour);
+    min = formatted_time(min);
+    sec = formatted_time(sec);
+
+    document.querySelector(".digital_clock").innerText = hour + " : " + min + " : " + sec + " " + sess;
+    document.querySelector(".date").innerText = day + "     " + dated + "-" + month + "-" + year;
+
+    setTimeout(current_time, 1000);
+
 }
 
-.digital_clock{
-  color: #66ff99;
-  font-family: 'Anton', sans-serif;
-  font-size: 56px;
-  text-align: center;
-  padding-top: 40px;
+function formatted_time(c){
+	if(c < 10) return "0" + c;
+	else return c;
 }
 
-.date{
-	color: #66ff99;
-  	font-size: 20px;
-  	font-family: 'Bangers', cursive;
-  	font-weight: 100;
-  	text-align: center;
-  	padding-bottom: 40px;
+function correct_day(numb){
+	if(numb == 0) return "Sunday";
+	else if(numb == 1) return "Monday";
+	else if(numb == 2) return "Tuesday";
+	else if(numb == 3) return "Wednesday";
+	else if(numb == 4) return "Thursday";
+	else if(numb == 5) return "Friday";
+	else if(numb == 6) return "Saturday";
 }
 
-.container{
-	margin-top: 15%;
-}
-
+current_time();
